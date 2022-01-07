d2e9ce047ed3915586f14d136da1b2c90a31d8d9
Shubham Vashishtha <16306985+shubhvash@users.noreply.github.com>
2022-01-08 00:54:12 +0530

    FE boilerplate

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-    return (
-        <Router>
-            {
-                <Switch>
-                    <Route exact path="/" component={''} />
-                    <Route exact path="/profile" component={''} />
-                </Switch>
-            }
-        </Router>
-    );
+    return <p>Hello World</p>;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
