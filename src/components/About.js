04939e8a6177c5c0c394bb27f650eb7f553ff942
saksha-collab <sakshamvashishtha700@gmail.com>
2022-01-08 12:40:59 +0530

    navbar_and_about

---
+++ src/components/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div>
+            {' '}
+            <div className="About">
+                <div className="secondhalf">
+                    <h2>DEV_FILES</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quisquam tenetur ea dicta,
+                        officiis doloribus commodi saepe, nam reiciendis corrupti eum blanditiis explicabo iusto quis
+                        ipsam architecto ducimus deserunt alias!
+                    </p>
+                </div>
+                <div className="firsthalf">
+                    {/* <img src="https://replit.com/@2K20CO393-Saksh/proj#img1.jpg" alt=""> */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
