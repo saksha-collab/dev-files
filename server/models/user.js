be155f2ab60a7abeaf3486070dabdea52b499a75
Shubham Vashishtha <16306985+shubhvash@users.noreply.github.com>
2022-01-08 23:05:54 +0530

    linting fix

--- server/models/user.js
+++ server/models/user.js
@@ -1,37 +1,36 @@
-const mongoose=require("mongoose");
-const userSchema=new mongoose.Schema({
-    githubid:{
-        type:String,
-        require:true
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    githubid: {
+        type: String,
+        require: true,
     },
-    githubrepos:{
-        type:String,
-        require:true
-    }
-    ,
-    githubfollowers:{
-        type:Number,
-        require:true
+    githubrepos: {
+        type: String,
+        require: true,
     },
-    githubfollowing:{
-        type:Number,
-        require:true
+    githubfollowers: {
+        type: Number,
+        require: true,
     },
-    codeforcesid:{
-        type:String,
-        require:true
+    githubfollowing: {
+        type: Number,
+        require: true,
     },
-    codeforcesrating:{
-        type:Number,
-        require:true
+    codeforcesid: {
+        type: String,
+        require: true,
     },
-    codechefid:{
-        type:String,
-        require:true
+    codeforcesrating: {
+        type: Number,
+        require: true,
     },
-    Linkedin:{
-        type:String,
-        require:true
-    }
-})
-module.exports=mongoose.model('user',userSchema);
+    codechefid: {
+        type: String,
+        require: true,
+    },
+    Linkedin: {
+        type: String,
+        require: true,
+    },
+});
+module.exports = mongoose.model('user', userSchema);
--- src/AddCred.jsx
+++ src/AddCred.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function AddCred() {
-    return (
-        <div>
-            
-        </div>
-    )
+    return <div></div>;
 }
 
-export default AddCred
+export default AddCred;
